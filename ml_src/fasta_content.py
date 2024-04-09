ed05b0486ad69fd8cc7fd1214b398590997059ef
Nikko Sanchez <n.sanchez7580@gmail.com>
2024-04-08 21:37:25 -0700

    minor edit

--- ml_src/fasta_content.py
+++ ml_src/fasta_content.py
@@ -12,20 +12,20 @@ class Fasta_Content:
 
   def __init__(self)->None:
     return
-  
+
   def parse_fasta_file(self, file:FileType)->None:
     """
     Parses an opened FASTA file for all of its IDs and sequences
     """
-    
+
     # get FASTA file name
     fasta_filename = str(file.name)
     self.fasta_filename = fasta_filename[fasta_filename.rfind('/')+1:fasta_filename.find('.')]
-    
+
     # parse FASTA file for sequence content
     for record in parse(file, 'fasta'):
       self.fasta_content[record.id] = record.seq
-  
+
   def get_fasta_filename(self)->str:
     return self.fasta_filename
 
@@ -34,7 +34,6 @@ class Fasta_Content:
     Returns all IDs from a FASTA file.
     """
     return self.fasta_content.keys()
-
 
   def get_sequences(self)->list:
     """
